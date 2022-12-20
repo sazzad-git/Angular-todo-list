34d0248f09590fb46b90626b5d3eada686d328b3
SazzadurRa <sazzadurrahman000@gmail.com>
2022-12-21 03:53:16 +0600

    complete todo list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-todo-list';
+
+  list: any[] = [];
+  addTask(item: string) {
+    this.list.push({ id: this.list.length, task: item });
+  }
+  removeTask(id: number) {
+    this.list = this.list.filter((item) => item.id !== id);
+    console.warn(id);
+  }
 }
